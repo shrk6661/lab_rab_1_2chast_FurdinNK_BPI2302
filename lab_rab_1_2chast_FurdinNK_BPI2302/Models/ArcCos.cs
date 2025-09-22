4cecc766d052c93af9f845ff563a0e28cf3bdb96
shrk6661 <shrk6661@gmail.com>
2025-09-22 17:41:27 +0500

    commit3

--- lab_rab_1_2chast_FurdinNK_BPI2302/Models/ArcCos.cs
+++ lab_rab_1_2chast_FurdinNK_BPI2302/Models/ArcCos.cs
@@ -1,20 +1,19 @@
-﻿namespace lab_rab_1_2chast_FurdinNK_BPI2302.Models
+﻿namespace lab_rab_1_2chast_FurdinNK_BPI2302.Models;
+
+public class ArcCos : Function
 {
-    public class ArcCos : Function
+    public ArcCos() : base("arccos") { }
+
+    public override double Value(double x)
     {
-        public ArcCos() : base("arccos") { }
-
-        public override double Value(double x)
-        {
-            if (x < -1 || x > 1)
-                throw new ArgumentOutOfRangeException(nameof(x), "x должен быть в промежутке [-1;1]");
-            return Math.Acos(x);
-        }
-
-        public override Function CreateDerivative()
-        {
-            return new AnalyticDerivative(xx => -1.0 / Math.Sqrt(1 - xx * xx), "arccos'");
-        }
-        // Info() не переопределяем — используется базовый
+        if (x < -1 || x > 1)
+            throw new ArgumentOutOfRangeException(nameof(x), "x должен быть в промежутке [-1;1]");
+        return Math.Acos(x);
     }
+
+    public override Function CreateDerivative()
+    {
+        return new AnalyticDerivative(xx => -1.0 / Math.Sqrt(1 - xx * xx), "arccos'");
+    }
+    // Info() не переопределяем — используется базовый
 }
